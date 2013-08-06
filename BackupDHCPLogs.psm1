7a59d38415357b6d3f2125d5d93a4cdca83c30f6
Dave Green <david.green@tookitaway.co.uk>
2013-08-06 14:30:30 +0100

    Add HelpMessage to mandatory parameters.

--- BackupDHCPLogs.psm1
+++ BackupDHCPLogs.psm1
@@ -34,7 +34,7 @@ Function Backup-DhcpLogs()
 
   [CmdletBinding()]
   Param(
-    [Parameter(Mandatory=$true,Position=1)][string]$Destination,
+    [Parameter(Mandatory=$true, Position=1, HelpMessage="The destination folder to back up the logs (and optionally, the configuration) to.")][string]$Destination,
     [string]$RetentionDays,
     [switch]$BackupConfig
   )
